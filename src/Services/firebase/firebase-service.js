3fbb2d65d14e4886f41e9249ac0c98912529ae13
diegoguilhermepassos <diegoguilhermepassos@github.com>
2021-09-17 16:15:48 -0300

    firebase configuration + CI/CD

---
+++ src/Services/firebase/firebase-service.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTjp5VrULjyJp8aEkRHgDdieIb-KEgVTw",
+    authDomain: "help-x-1378d.firebaseapp.com",
+    projectId: "help-x-1378d",
+    storageBucket: "help-x-1378d.appspot.com",
+    messagingSenderId: "602193496441",
+    appId: "1:602193496441:web:87bfa23198b6218b98c7a8",
+    measurementId: "G-ZZ6NGDQPSY"
+  };
+
+  const app = initializeApp(firebaseConfig);
